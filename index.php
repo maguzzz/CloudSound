3f48422e838360ed1cccbdd93ab38c86ab66acc7
albinkizh <albinkizhakkedath3@gmail.com>
2024-09-11 10:39:16 +0200

    fixed merge conflict

--- index.php
+++ index.php
@@ -15,42 +15,5 @@ Flight::route('GET  /', array($mainController, 'index'));
 Flight::route('GET  /upload', array($uploadController, 'index'));
 Flight::route('POST /upload', array($uploadController, 'upload'));
 
-<body>
-    <?php
-        Flight::route('/', function () {
-        });
 
-        //DBsubmitSong(1,"ss.png","ss.mp3"," 	SS","NEW SONG BY KEN CARSON","Rap","Ken Carson","McFlurry","Pluto");
-        DBGetSongs('s') ;
-        Flight::start();
-
-    ?>
-
-
-    <form id="container" action="/Cloudsound" method="post">
-        
-        <input id="songImageUploader" type="file" accept="image/*"> 
-        <div id="imagerAndImageUploader">
-            <div id="songImage">
-
-            </div>
-            <div id="imageUploaderContainer">
-
-                <label id="labelForUploaderImage" for="songImageUploader">Upload File</label>
-            </div>
-
-        </div>
-        
-
-        <div id="nameAndDescription">
-            <input type="text" id="songName" placeholder="Name"><br>
-            <input type="text" id="songDescription" placeholder="Description"><br>
-            <input type="submit" id="songUpload" value="Upload">
-        </div>
-    </form>
-
-
-
-</body>
-
-</html>
+Flight::start();
