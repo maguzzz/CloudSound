f5bc0a4e5d9766f8e2319aac6814630edb13b08a
Maguzzz <markusm.touray@gmail.com>
2024-09-13 09:13:08 +0200

    Image Preview Upload

--- Js/uploadSong.js
+++ Js/uploadSong.js
@@ -8,3 +8,24 @@ fileInput.addEventListener('change', function() {
         label.textContent = `${fileName}`;
     }
 });
+
+
+
+document.getElementById('songImageUploader').addEventListener('change', function(event) {
+    const songImageDiv = document.getElementById('songImage');
+    const file = event.target.files[0];
+
+    if (file && file.type.startsWith('image/')) {
+        const reader = new FileReader();
+        
+        reader.onload = function(e) {
+            songImageDiv.style.backgroundImage = `url(${e.target.result})`;
+            songImageDiv.style.backgroundSize = 'cover';
+            songImageDiv.style.backgroundPosition = 'center';
+        };
+
+        reader.readAsDataURL(file);
+    } else {
+        alert('Please upload a valid image file.');
+    }
+});
